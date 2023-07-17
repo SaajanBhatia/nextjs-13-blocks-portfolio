8749e3bf0749a4cabd9ef4074dd77991bb7df73c
Saajan Bhatia <saajansbhatia@gmail.com>
2023-07-17 23:02:43 +0100

    latest

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -45,7 +45,7 @@ export default function Hero() {
                     </div>
                     <div className="hidden lg:flex lg:gap-x-12">
                         {socials && socials.map((item) => (
-                            <a key={item.name} href={item.url} className="text-sm font-semibold leading-6 text-gray-900">
+                            <a key={item.name} href={item.url} className="text-sm font-semibold leading-6 text-gray-900" target='_blank'>
                                 {item.name}
                             </a>
                         ))}
@@ -96,6 +96,7 @@ export default function Hero() {
                                         <a
                                             key={item.name}
                                             href={item.url}
+                                            target='_blank'
                                             className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                                         >
                                             {item.name}
