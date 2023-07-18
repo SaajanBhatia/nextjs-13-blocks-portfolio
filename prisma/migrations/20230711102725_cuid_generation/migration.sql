cd7fa24a2da7baae397da510d4b5d07f406c7380
Saajan Bhatia <saajansbhatia@gmail.com>
2023-07-18 10:24:46 +0100

    Production Ready Prisma Migration

--- prisma/migrations/20230711102725_cuid_generation/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `block` MODIFY `url` VARCHAR(191) NULL;
--- prisma/migrations/20230718092335_production_ready/migration.sql
+++ prisma/migrations/20230718092335_production_ready/migration.sql
@@ -24,3 +24,13 @@ CREATE TABLE `Block` (
     UNIQUE INDEX `Block_id_key`(`id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Socials` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `url` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Socials_id_key`(`id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
